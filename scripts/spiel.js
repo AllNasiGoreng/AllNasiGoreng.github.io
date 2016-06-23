723d017605a80e851abd1aadd65b40a417498ac7
Linda <LiFaytheGoblin@googlemail.com>
2016-06-23 11:16:20 +0200

    spiel anfang

--- scripts/spiel.js
+++ scripts/spiel.js
@@ -1,1 +1,13 @@
 //Spiel
+
+document.onload = function () {
+    //import sprites: canvas, nasi goreng, animal, text:points
+    var canvas = document.getElementById("game"),
+        
+        points = 0, //Punktestand
+        
+        text = canvas.getContext("2d"); //Punktestandanzeige
+    text.font = "20px Arial";
+    text.fillText("Points: " + points, 10, 10);
+    
+};
