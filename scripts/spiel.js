c3e03ea65c984fe883b1475b6388b3060058f78a
Linda <LiFaytheGoblin@googlemail.com>
2016-06-23 15:06:09 +0200

    spiel angefangen

--- scripts/spiel.js
+++ scripts/spiel.js
@@ -1,13 +1,55 @@
 //Spiel
 
-document.onload = function () {
-    //import sprites: canvas, nasi goreng, animal, text:points
-    var canvas = document.getElementById("game"),
-        
-        points = 0, //Punktestand
-        
-        text = canvas.getContext("2d"); //Punktestandanzeige
-    text.font = "20px Arial";
-    text.fillText("Points: " + points, 10, 10);
+window.onload = function () {
     
+    //VORBEREITEN: canvas, context, points, pointText, player, food
+
+    var canvas = document.getElementsByTagName("canvas"),
+        ctx = canvas.getContext("2d"),
+
+        points = 0,
+
+        pointText = {
+            x: 30,
+            y: 30,
+            tex: "points: " + points
+        },
+
+        player = {
+            x: canvas.width / 2.5,
+            y: canvas.height / 1.5
+        },
+
+        food = {
+            x: canvas.width / 4,
+            y: 30,
+            speed: 50
+        };
+
+    //USER INPUT VERWERTEN: onmousedown - onmouseup
+
+    //START
+
+    var start = function () {
+        food.x = 30;
+        food.y = 50;
+    };
+
+    //AKTUALISIEREN
+
+    var update = function () {
+        if (/*food geklickt*/) {
+            //Update Points
+            points++;
+            pointText.tex = "points: " + points;
+            
+            //Update Position of Food
+            food.x = /*random*/;
+            food.y = /*random*/;
+        }
+    }
+    
+    //RENDERN
+
+
 };
