02cd9a0fd14a8d0d3295144b93676d335fcd189f
GunBlade92 <al_soejoedi@live.com>
2016-07-12 17:39:50 +0200

    feedback prüfen

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,1 +1,14 @@
 //Benutzerdaten entgegen nehmen
+/*function myFunction() {
+    document.getElementById("form1").submit();
+}*/
+/*function myFunction() {
+    var str1 = document.getElementById("vorname").innerHTML,
+        str2 = document.getElementById("nachname").innerHTML;
+    if(str1 ==  ) {
+        alert('Bitte keine Zahl in ihrem Name.');
+        return false;
+    }else{
+        return true;
+    }
+}*/
