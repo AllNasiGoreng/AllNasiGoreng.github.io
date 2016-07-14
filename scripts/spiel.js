4cd9758b1674108bfefd61d900ea6c4e9f4baef2
Linda <LiFaytheGoblin@googlemail.com>
2016-07-14 12:16:44 +0200

    menü gefixt
    mobil und spiel stören sich nicht mehr

--- scripts/spiel.js
+++ scripts/spiel.js
@@ -1,45 +1,50 @@
 window.onload = function () {
 
-    var intervallID = null,
-        instructions = document.getElementById("instructions"),
-        food = document.getElementById("food"),
-        points = 0,
-        pText = document.getElementById("punkte"),
-        x = 240,
-        y = 140,
-        left = x + "px",
-        top = y + "px";
-    
-    food.style.left = left;
-    food.style.top = top;
-    
-    //react to mobile??
-    instructions.innerHTML = "Bewege den Mauszeiger über das Nasi Goreng, um es einzusammeln."
+    instructions = document.getElementById("instructions");
 
-    function updatePoints() {
-        pText.innerHTML = "Punkte: " + points;
-    }
+    if (window.innerWidth > 670) {
+        var intervallID = null,
+            food = document.getElementById("food"),
+            points = 0,
+            pText = document.getElementById("punkte"),
+            x = 240,
+            y = 140,
+            left = x + "px",
+            top = y + "px";
 
-    food.addEventListener("mouseover", startPlay, false);
+        food.style.left = left;
+        food.style.top = top;
 
-    function startPlay() {
-        if (intervallID === null) {
-            intervallID = setInterval(function () {
-                //animation
-                
-                left = x + "px";
-                top = y + "px";
+        //react to mobile??
+        instructions.innerHTML = "Bewege den Mauszeiger über das Nasi Goreng, um es einzusammeln.";
 
-                x = Math.random() * 480;
-                y = Math.random() * 280;
-
-                food.style.left = left;
-                food.style.top = top;
-            }, 650);
-        } else {
-            points += 1;
-            updatePoints();
-
+        function updatePoints() {
+            pText.innerHTML = "Punkte: " + points;
         }
+
+        food.addEventListener("mouseover", startPlay, false);
+
+        function startPlay() {
+            if (intervallID === null) {
+                intervallID = setInterval(function () {
+                    //animation
+
+                    left = x + "px";
+                    top = y + "px";
+
+                    x = Math.random() * 480;
+                    y = Math.random() * 280;
+
+                    food.style.left = left;
+                    food.style.top = top;
+                }, 650);
+            } else {
+                points += 1;
+                updatePoints();
+
+            }
+        }
+    } else {
+        instructions.innerHTML = "Dieses Spiel ist leider nicht auf mobilen oder zu kleinen Bildschirmen verfügbar.";
     }
 };
