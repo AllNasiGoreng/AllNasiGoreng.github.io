3d708bbceec8101a2e23e9ceee38dbed17f0dcaa
GunBlade92 <al_soejoedi@live.com>
2016-07-18 20:17:04 +0200

    feedback (endlich) eingeprüft

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,14 +1,35 @@
 //Benutzerdaten entgegen nehmen
-/*function myFunction() {
-    document.getElementById("form1").submit();
-}*/
-/*function myFunction() {
-    var str1 = document.getElementById("vorname").innerHTML,
-        str2 = document.getElementById("nachname").innerHTML;
-    if(str1 ==  ) {
-        alert('Bitte keine Zahl in ihrem Name.');
-        return false;
-    }else{
-        return true;
-    }
-}*/
+
+ function removeElementsByClass(className){
+        elements = document.getElementsByClassName(className);
+        while(elements.length > 0){
+          elements[0].parentNode.removeChild(elements[0]);
+        }
+      }    
+    
+      function flagError(field, prependText){
+        var message = prependText + " kann nicht leer sein!";
+        form[field].insertAdjacentHTML('afterend', '<span class="error">' + message + '</span>');
+      } // error Nachtricht
+    
+      function checkEmpty(field, prependText){
+        if (form[field].value === ""){
+          flagError(field, prependText);        
+          isValidSubmission = false;
+        }
+      }
+    
+      function validateForm() {
+        removeElementsByClass("error");
+        
+        checkEmpty("vorname", "Vorname"); //Vorname validieren
+        checkEmpty("nachname", "Nachname"); //Nachname validieren
+        checkEmpty("email", "E-mail"); //E-mail validieren
+        
+        return isValidSubmission;
+      }
+      
+      var form = document.getElementById("feedback"),
+          isValidSubmission = true;
+          
+      form.onsubmit = validateForm;
