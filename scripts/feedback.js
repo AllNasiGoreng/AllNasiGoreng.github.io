93e7c24d06d25700108a7d4dfedb9cb57c787ac5
GunBlade92 <al_soejoedi@live.com>
2016-07-20 22:30:28 +0200

    feedback 2

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,34 +1,13 @@
-function removeElementsByClass(className) {
- 
-    elements = document.getElementsByClassName(className);
-        while(elements.length > 0){
-          elements[0].parentNode.removeChild(elements[0]);
+function validateForm() {
+    'use strict';
+    var a = document.forms['feedback'].elements['vorname'].value;
+    function allLetter(a) {
+        var letters = /^[A-Za-z]+$/;
+        if(a.value.match(letters)){
+            return true;
+        }else{
+            alert("Bitte keine Zahl eintippen.");
+            return false;
         }
-}    
-    
-      function flagError(field, prependText){
-        var message = prependText + " kann nicht leer sein!";
-        form[field].insertAdjacentHTML('afterend', '<span class="error">' + message + '</span>');
-      } // error Nachtricht
-    
-      function checkEmpty(field, prependText){
-        if (form[field].value === ""){
-          flagError(field, prependText);        
-          isValidSubmission = false;
-        }
-      }
-    
-      function validateForm() {
-        removeElementsByClass("error");
-        
-        checkEmpty("vorname", "Vorname"); //Vorname validieren
-        checkEmpty("nachname", "Nachname"); //Nachname validieren
-        checkEmpty("email", "E-mail"); //E-mail validieren
-        
-        return isValidSubmission;
-      }
-      
-      var form = document.getElementById("feedback"),
-          isValidSubmission = true;
-          
-      form.onsubmit = validateForm;
+    }
+}
