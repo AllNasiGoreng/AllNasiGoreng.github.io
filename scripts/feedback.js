16c5ce23823b9fab744a38b3fa75d32804b80ae0
Linda <LiFaytheGoblin@googlemail.com>
2016-07-21 10:14:47 +0200

    verbessert

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,41 +1,37 @@
 window.onload = function () {
     'use strict';
-    
+
     var button = document.getElementsByName("submit")[0];
 
-    button.addEventListener('click', validate, false);
+    button.onclick = validate;
 
     function validate() {
         var vorname = document.forms['feedback']['vorname'].value,
-        nachname = document.forms['feedback']['nachname'].value,
-        email = document.forms['feedback']['email'].value;
+            nachname = document.forms['feedback']['nachname'].value,
+            email = document.forms['feedback']['email'].value,
+            message = "",
+            atLeastOneInvalid = 0,
+            containsNumbers = 0,
+            numbers = /[0-9]+/;
         
-        var message = "",
-            atLeastOneInvalid = 0;
-
-                var containsNumbers = 0,
-            numbers = /^[0-9]+$/;
         //emptyness
         if (vorname === "" || numbers.test(vorname)) {
-            console.log(vorname);
             message = message + "Dein Vorname ist ungültig. ";
             atLeastOneInvalid = 1;
         }
-        
+
         if (nachname === "" || numbers.test(nachname)) {
-            console.log(nachname);
             message = message + "Dein Nachname ist ungültig. ";
             atLeastOneInvalid = 1;
         }
-        
+
         if (email === "") {
-            console.log(email);
             message = message + "Gib bitte deine Emailadresse an. ";
             atLeastOneInvalid = 1;
         }
-        
+
         //alert message
-        if (atLeastOneInvalid === 1) {
+        if (atLeastOneInvalid) {
             alert(message);
         }
 
