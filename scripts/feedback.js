a7b9a5bfe1e9f6458de5d8d2301f8de5a40cad9c
Linda <LiFaytheGoblin@googlemail.com>
2016-07-21 09:33:57 +0200

    feedback

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -3,14 +3,41 @@ window.onload = function() {
 <<<<<<< Updated upstream
     var vorname = document.forms['feedback']['vorname'].value,
         nachname = document.forms['feedback']['nachname'].value,
+        email = document.forms['feedback']['email'].value,
         button = document.getElementsByName("submit")[0];
 
     button.addEventListener('click', validate, false);
     
     function validate() {
+        var message = "",
+            atLeastOneInvalid = 0;
+        
+        //emptyness
         if(vorname == "") {
-            alert("Gib bitte deinen Vornamen an");
+            message = message + "Gib bitte deinen Vornamen an. ";
+            atLeastOneInvalid = 1;
         }
+        if(nachname == "") {
+            message = message + "Gib bitte deinen Nachnamen an. ";
+            atLeastOneInvalid = 1;
+        }
+        if(email == "") {
+            message = message + "Gib bitte deine Emailadresse an. ";
+            atLeastOneInvalid = 1;
+        }
+        
+        //symbols
+        //vorname nachname
+        var containsNumbers = 0;
+        for(var i = 0; i < vorname.length(); i++) {
+            containsNumbers = !(isNaN(vorname));
+        }
+        
+        //alert message
+        if(atLeastOneInvalid) {
+            alert(message);
+        }
+        
     }
     
     /*
